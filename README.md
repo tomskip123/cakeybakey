74dab7cf22a28f6e8bb3700880ff6e3f2805ce2f
tom peacock <tom.peacock@thejustbrand.co.uk>
2021-07-30 08:27:32 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Dear adudicators of my cakeybakey project.
+
+I figured this would be a little simple, so I decided to bump it up a little.
+
+I've decided to include frameworks that include testing out of the box to make things a little more simple, 
+
+the frameworks are nestjs and nextjs
+
+nextjs is using material-ui
+
+oh and for the lols I wacked it in a docker container and included a build script just for lols.
---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+import { Grid, Typography } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Typography variant="h1">
+          Welcome to cakey!
+        </Typography>
+
+        <Grid style={{ padding: '20px 0 20px 0' }} container>
+
+          <Grid item>
+            
+          </Grid>
+
+        </Grid>
+      </header>
+    </div>
+  );
+}
+
+export default App;
